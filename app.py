66210cb73c4fbb23be19197fef9993bd133e3371
lkdmttg7 <inprovertmer07@gmail.com>
2021-05-18 23:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import sys
+import time
+import random
 
+def get_sentence(self):
+    f = open('sentences.txt').read()
+    sentences = f.split('\n')
+    sentence = random.choice(sentences)
+    return sentence
