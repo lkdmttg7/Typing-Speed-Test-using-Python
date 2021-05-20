b728dde606a7782f46b44c269aa18310f7164f3d
lkdmttg7 <inprovertmer07@gmail.com>
2021-05-20 23:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ def get_sentence(self):
     sentences = f.split('\n')
     sentence = random.choice(sentences)
     return sentence
+def draw_text(self, screen, msg, y ,fsize, color):
+    font = pygame.font.Font(None, fsize)
+    text = font.render(msg, 1,color)
+    text_rect = text.get_rect(center=(self.w/2, y))
+    screen.blit(text, text_rect)
+    pygame.display.update()
